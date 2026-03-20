8296fb87462a34f145c6ba6b822b54768c9115d0
Redaction-F <koebijakobiteorhang@gmail.com>
2026-03-20 09:59:00 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Grid from "./components/grid";
+import Self from "./components/self";
+
+function App() {
+
+  return (
+    <main className="container">
+      <Grid></Grid>
+      <Self></Self>
+    </main>
+  );
+}
+
+export default App;
